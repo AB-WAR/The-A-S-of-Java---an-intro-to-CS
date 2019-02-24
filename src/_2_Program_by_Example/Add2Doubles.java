6b871c7fdf243d2c46f729047a1a911d9b3610d4
AB-WAR <41446348+AB-WAR@users.noreply.github.com>
2019-02-24 19:12:10 +0530

    Add Add2Integers and Add2Doubles
    Programming Idioms and patterns

---
+++ src/_2_Program_by_Example/Add2Doubles.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package _2_Program_by_Example;
+
+/**
+ * @author ABWAR
+ *
+ */
+
+import acm.program.*;
+
+public class Add2Doubles extends ConsoleProgram {
+	
+	public void run() {
+		println("This program adds two numbers.");
+		double n1 = readDouble("Enter n1: ");
+		double n2 = readDouble("Enter n2: ");
+		double total = n1 + n2;
+		println("The total is " + total + ".");
+	}
+}
---
+++ src/_2_Program_by_Example/Add2Integers.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package _2_Program_by_Example;
+
+/**
+ * @author ABWAR
+ *
+ */
+
+import acm.program.*;
+
+public class Add2Integers extends ConsoleProgram {
+	
+	public void run() {
+		println("This program adds two integers.");
+		int n1 = readInt("Enter n1: ");
+		int n2 = readInt("Enter n2: ");
+		int total = n1 + n2;
+		println("The total is " + total + ".");
+	}
+}
