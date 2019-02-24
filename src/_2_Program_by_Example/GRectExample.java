517519f227452699b744d8a1d62657cb70900c2c
AB-WAR <41446348+AB-WAR@users.noreply.github.com>
2019-02-25 00:08:56 +0530

    Add GRectExample

---
+++ src/_2_Program_by_Example/GRectExample.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package _2_Program_by_Example;
+
+/**
+ * @author ABWAR
+ *
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*;
+
+public class GRectExample extends GraphicsProgram {
+	
+	public void run() {
+		GRect rect = new GRect(100, 50, 125, 60);
+		rect.setFilled(true);
+		rect.setColor(Color.RED);
+		add(rect);
+	}
+
+}
--- src/_2_Program_by_Example/HelloProgram.java
+++ src/_2_Program_by_Example/HelloProgram.java
@@ -3,15 +3,24 @@
  */
 package _2_Program_by_Example;
 
+import java.awt.Color;
+import acm.graphics.*;
+import acm.program.*;
+
 /**
  * @author ABWAR
  *
  */
-import acm.graphics.*;
-import acm.program.*;
 
 public class HelloProgram extends GraphicsProgram {
 	public void run() {
-		add(new GLabel("hello, world", 100, 75));
+		  add(new GLabel("hello, world", 100, 75));
+		
+		/*
+		  GLabel msg = new GLabel("hello, world", 100, 75);
+		  msg.setFont("Helvetica-24");
+		  msg.setColor(Color.RED);
+		  add(msg);
+		 */
 	}
 }
