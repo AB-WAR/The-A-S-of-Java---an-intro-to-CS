0f543318ec6c5268a8c0c5efba5a100dfba0e466
AB-WAR <41446348+AB-WAR@users.noreply.github.com>
2019-03-03 23:27:35 +0530

    Create DigitSum.java

---
+++ src/_4_Program_by_Example/DigitSum.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package _4_Program_by_Example;
+
+/**
+ * @author ABWAR
+ *
+ */
+
+import acm.program.*;
+
+public class DigitSum extends ConsoleProgram {
+	
+	public void run() {
+		println("This program sums the digits in an integer."); 
+		int n = readInt("Enter a positive integer: "); 
+		int dsum = 0; 
+		while (n > 0) { 
+			dsum += n % 10; 
+			n /= 10; 
+		} 
+		println("The sum of the digits is " + dsum);
+	}
+
+}
