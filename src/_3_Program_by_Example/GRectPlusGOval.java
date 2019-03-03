ddc17ebda6dd62ec8328680301503a28e516f998
AB-WAR <41446348+AB-WAR@users.noreply.github.com>
2019-03-03 22:40:09 +0530

    Create GRectPlusGOval.java

---
+++ src/_3_Program_by_Example/GRectPlusGOval.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package _3_Program_by_Example;
+
+/**
+ * @author ABWAR
+ *
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*;
+
+public class GRectPlusGOval extends GraphicsProgram {
+	
+	public void run() {
+		double x = (getWidth() - FIGURE_WIDTH) / 2;
+		double y = (getHeight() - FIGURE_HEIGHT) / 2;
+		GRect rect = new GRect(x, y, FIGURE_WIDTH, FIGURE_HEIGHT);
+		rect.setFilled(true);
+		rect.setColor(Color.RED);
+		add(rect);
+		GOval oval = new GOval(x, y, FIGURE_WIDTH, FIGURE_HEIGHT);
+		oval.setFilled(true);
+		oval.setFillColor(Color.GREEN);
+		add(oval);
+	}
+	
+	private static final double FIGURE_WIDTH = 125;
+	private static final double FIGURE_HEIGHT = 75;
+
+}
