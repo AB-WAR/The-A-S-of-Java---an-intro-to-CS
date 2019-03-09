0892e2495b29b932895094ad40a93a4bffdd20ef
AB-WAR <41446348+AB-WAR@users.noreply.github.com>
2019-03-09 07:15:31 +0530

    Update FilledRect.java

--- src/_6_Program_by_Example/FilledRect.java
+++ src/_6_Program_by_Example/FilledRect.java
@@ -9,10 +9,16 @@ package _6_Program_by_Example;
  */
 
 import acm.graphics.*;
+import acm.program.*;
 import java.awt.*;
 
 public class FilledRect extends GRect {
 	
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 /** Creates a new FilledRect with the specified bounds. */
 	public FilledRect(double x, double y, double width, double length) {
 		super(x, y, width, length);
